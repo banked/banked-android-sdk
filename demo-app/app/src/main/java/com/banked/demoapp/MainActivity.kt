43b432fe952c1b0aaeb8b18c49a103075b86b38b
kristinaSl <kristina.sl.nenkova@gmail.com>
2020-06-29 10:05:54 +0100

    Change version

--- demo-app/app/src/main/java/com/banked/demoapp/MainActivity.kt
+++ demo-app/app/src/main/java/com/banked/demoapp/MainActivity.kt
@@ -3,7 +3,6 @@ package com.banked.demoapp
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.banked.checkout.CheckoutActivity
